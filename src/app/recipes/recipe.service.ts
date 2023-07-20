f9b1ec6dfd7cc5f2940ee269bf66ce7c1065c36c
Nicolas Peña <103202106+Nicolasp-dev@users.noreply.github.com>
2023-07-20 08:55:03 -0500

    Firebase DB added, Get Put Services added

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,22 +8,30 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      'A test recipe',
-      'Test Description',
-      'https://img.freepik.com/free-photo/high-angle-delicious-brazilian-food-composition_23-2148739223.jpg?w=826&t=st=1678895314~exp=1678895914~hmac=fbd7f0129a58eddf7020125f1f80acabf1203927a6c13ce979e5e47bf62b4dec',
-      [new Ingredient('Meat', 1), new Ingredient('Bread', 2)]
-    ),
-    new Recipe(
-      'Second recipe',
-      'Second Description',
-      'https://img.freepik.com/free-photo/high-angle-delicious-brazilian-food-composition_23-2148739223.jpg?w=826&t=st=1678895314~exp=1678895914~hmac=fbd7f0129a58eddf7020125f1f80acabf1203927a6c13ce979e5e47bf62b4dec',
-      [new Ingredient('Beaf', 5), new Ingredient('French fries', 20)]
-    ),
-  ];
+  
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'A test recipe',
+  //     'Test Description',
+  //     'https://img.freepik.com/free-photo/high-angle-delicious-brazilian-food-composition_23-2148739223.jpg?w=826&t=st=1678895314~exp=1678895914~hmac=fbd7f0129a58eddf7020125f1f80acabf1203927a6c13ce979e5e47bf62b4dec',
+  //     [new Ingredient('Meat', 1), new Ingredient('Bread', 2)]
+  //   ),
+  //   new Recipe(
+  //     'Second recipe',
+  //     'Second Description',
+  //     'https://img.freepik.com/free-photo/high-angle-delicious-brazilian-food-composition_23-2148739223.jpg?w=826&t=st=1678895314~exp=1678895914~hmac=fbd7f0129a58eddf7020125f1f80acabf1203927a6c13ce979e5e47bf62b4dec',
+  //     [new Ingredient('Beaf', 5), new Ingredient('French fries', 20)]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,39 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RecipeService } from './../recipes/recipe.service';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataStorageService {
-  constructor( private http : HttpClient){}
+  url = 'https://cooking-ang-af0fd-default-rtdb.firebaseio.com/';
+
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(`${this.url}/recipes.json`, recipes).subscribe((response) => {
+      console.log(response);
+    });
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>(`${this.url}/recipes.json`)
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
+      .subscribe((response) => {
+        this.recipeService.setRecipes(response);
+      });
+  }
 }
